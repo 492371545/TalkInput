471bacdda12f2bf25fe837fb1fef75264dbf36fc
492371545 <xmyxumengying@hotmail.com>
2014-10-21 09:35:22 +0800

    Initial Commit

---
+++ TalkInput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TalkInput
+//
+//  Created by Mengying Xu on 14-10-21.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
